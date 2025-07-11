cc385039ba63285d53f0271ace2ac5619d97a172
papilooz <manriquequervie@gmail.com>
2025-07-11 22:17:11 +0800

    feat: adding firebase database connection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -37,11 +38,17 @@ android {
 
 dependencies {
 
+    implementation(platform(libs.firebase.bom))  // Firebase BoM (add this line)
+
+    implementation(libs.firebase.auth)           // Firebase Auth (add this line)
+    implementation(libs.firebase.firestore)      // Firestore (already there)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
